a16d7a64ebca05509ffc8fdd06dba58fe78e2e16
damien <dbeauseigneur@gmail.com>
2019-03-23 21:31:19 +0100

    maj composant

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ fine-upload-file
 ================
 
 A Symfony project created on May 22, 2017, 2:52 pm.
+
+created for integrate completely fine uploader js as a field in symfony form
---
+++ src/AppBundle/Twig/Extension/head.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class head extends \Twig_Extension
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getName()
+	{
+		return 'head';
+	}
+}
